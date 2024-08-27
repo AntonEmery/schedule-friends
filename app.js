9b56681275f52312539f5124715e0a8f94c16a90
Anton Emery <antonemery@gmail.com>
2024-08-27 15:02:54 -0700

    Update main app.js to use ES module style imports
    Add body-parser module
    
    Sync database before app boots up and seed user data
    Send user data to browser on home route GET request

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { sequelize } from './database.js';
+import { User } from './Models/User.js';
+import { seedUsers } from "./seed-data.js"
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+sequelize.sync()
+.then(async () => {
+  console.log('Database synced');
+  await seedUsers();
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+})
+.catch((err) => {
+  console.error('Error syncing database', err);
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.get('/', async (req, res) => {
+  const users = await User.findAll();
+  res.json(users);
 });
